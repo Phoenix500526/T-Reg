293e94577eb0a523de732be34d4e6aea51513832
Phoenix <Phoenix500526@163.com>
2020-12-01 23:40:32 +0800

    添加标签类 noncopyable

---
+++ noncopyable.h
@@ -0,0 +1,16 @@
+#ifndef TREG_NONCPOYABLE_H_
+#define TREG_NONCPOYABLE_H_
+namespace treg {
+class noncopyable {
+ public:
+  noncopyable(const noncopyable&) = delete;
+  void operator=(const noncopyable&) = delete;
+
+ protected:
+  //将构造函数和析构函数都声明为 protected
+  //可将实例化的过程限制在子类当中，从而避免外部实例化 noncopyable 对象
+  noncopyable() = default;
+  ~noncopyable() = default;
+};
+}  // namespace treg
+#endif  // TREG_NONCPOYABLE_H_
