5013b3f7ab1c5fbfe04d25f166a7d7750162697a
Phoenix <Phoenix500526@163.com>
2020-12-01 16:45:54 +0800

    修改编译脚本

--- build.sh
+++ build.sh
@@ -6,17 +6,15 @@ SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-./build}
 BUILD_TYPE=${BUILD_TYPE:-debug}
 
-mkdir -p $BUILD_DIR \
-  && cd $BUILD_DIR \
-  && conan install .. \
-            --build missing     \
-            -s compiler=clang   \
+mkdir -p $BUILD_DIR 
+cd $BUILD_DIR 
+conan install .. --build missing    \
+            -s compiler=clang       \
             -s compiler.version=3.8 \
-            -s compiler.libcxx=libstdc++11  \
-  && cmake .. \
-           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-           -DCMAKE_CXX_COMPILER=clang++ \
-  && make $*
+            -s compiler.libcxx=libstdc++11  
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=clang++ 
+make clang-format
+make $*
 
 # Use the following command to run all the unit tests
 # at the dir $BUILD_DIR/$BUILD_TYPE :
